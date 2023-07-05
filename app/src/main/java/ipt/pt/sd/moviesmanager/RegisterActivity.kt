30ba6babae5a893ec4486b675e5e2665d163f4a3
CitrinoOps <80701608+CitrinoOps@users.noreply.github.com>
2023-07-05 01:13:21 +0100

    First Commit Pedro
    Movie Class
    Main Activity
    Activity main xml
    Some Buttons

---
+++ app/src/main/java/ipt/pt/sd/moviesmanager/RegisterActivity.kt
@@ -0,0 +1,15 @@
+package com.example.retrofitmoshi
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class RegisterActivity : AppCompatActivity() {
+
+
+}
+
+class User(val uid:String, val username: String)
