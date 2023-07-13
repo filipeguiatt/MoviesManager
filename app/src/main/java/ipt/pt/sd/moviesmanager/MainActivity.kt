b3e990f37734feaa8e16acccf00a09c1d7854919
filipeguiatt <80701711+filipeguiatt@users.noreply.github.com>
2023-07-13 23:46:17 +0100

    Teste

--- app/src/main/java/ipt/pt/sd/moviesmanager/MainActivity.kt
+++ app/src/main/java/ipt/pt/sd/moviesmanager/MainActivity.kt
@@ -2,8 +2,10 @@ package ipt.pt.sd.moviesmanager
 
 
 import androidx.appcompat.app.AppCompatActivity
+import ipt.pt.sd.moviesmanager.models.Movie
 
 
 class MainActivity : AppCompatActivity() {
 
+    val mList = mutableListOf<Movie>()
 }
