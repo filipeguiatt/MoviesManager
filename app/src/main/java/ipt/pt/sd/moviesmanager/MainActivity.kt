0d6db094a738712f7a299ebe3bef748d6598b85d
filipeguiatt <80701711+filipeguiatt@users.noreply.github.com>
2023-07-14 01:07:21 +0100

    Main Activity (Call Movies)
    Falta criar a API

--- app/src/main/java/ipt/pt/sd/moviesmanager/MainActivity.kt
+++ app/src/main/java/ipt/pt/sd/moviesmanager/MainActivity.kt
@@ -1,11 +1,56 @@
 package ipt.pt.sd.moviesmanager
 
 
+import android.os.Bundle
+import android.telecom.Call
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
 import ipt.pt.sd.moviesmanager.models.Movie
+import ipt.pt.sd.moviesmanager.models.Search
+import javax.security.auth.callback.Callback
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Response
 
 
 class MainActivity : AppCompatActivity() {
 
+    //Declaração das listas de filmes
     val mList = mutableListOf<Movie>()
+    val fList = mutableListOf<Movie>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        callMovies()
+
+        //Função para buscar filmes
+        fun callMovies() {
+            var call = API.create().getData()
+
+            call.enqueue(object : Callback<Search> {
+                override fun onFailure(call: Call<Search>, t: Throwable) {
+                    Log.e("onFailure error", call.request().url().toString())
+                    Log.e("onFailure error", t.message!!)
+                }
+
+                override fun onResponse(call: Call<Search>, response: Response<Search>) {
+                    response.body()?.let {
+                        // Ocultar o símbolo de carregamento
+                        homeprogress1.visibility = ViewPager.GONE
+
+                        mList.clear()
+                        mList.addAll(it.results)
+
+                        fList.clear()
+                        fList.addAll(it.results)
+
+                        list.adapter?.notifyDataSetChanged()
+                    }
+                }
+            })
+        }
+
+    }
 }
---
+++ app/src/main/java/ipt/pt/sd/moviesmanager/models/Search.kt
@@ -0,0 +1,4 @@
+package ipt.pt.sd.moviesmanager.models
+
+class Search(var page: Int, var results: List<Movie>) {
+}
